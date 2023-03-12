60de9e2740e51761cb8b7ec91059486097594473
Krish Panchani <74005086+Krish-Panchani@users.noreply.github.com>
2023-03-12 11:50:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function mobileMenu() {
+    var y = document.getElementById('mobileMenu'); {
+        if (y.style.display == "block") {
+            y.style.display = "none";
+        }
+        else {
+            y.style.display = "block";
+        }
+
+    }
+}
